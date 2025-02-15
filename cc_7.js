a3833468f59acd3ba939e96f4378dfe607e5c0cd
Fernando <mhfernando778@gmail.com>
2025-02-15 17:58:29 -0500

    Task 2 - Employee Hourly Wage with function expression

--- cc_7.js
+++ cc_7.js
@@ -7,3 +7,14 @@
 
  console.log(`Final Invoice Amount: $${calculateInvoice(100, 0.08, 5)}`); // logging the function with specified values
  console.log(`Final Invoice Amount: $${calculateInvoice(500, 0.1, 20)}`); // logging the function with specified values
+
+
+ // Task 2 - Employee Hourly Wage Calculation //
+
+ let calculateHourlyWage = function(salary, hoursPerWeek) {
+   let hourlyWage = salary / (hoursPerWeek * 52);
+   return hourlyWage.toFixed(2);
+ };
+
+ console.log(`Employee Hourly Wage: $${calculateHourlyWage(52000, 40)}`);
+ console.log(`Employee Hourly Wage: $${calculateHourlyWage(75000, 35)}`);
