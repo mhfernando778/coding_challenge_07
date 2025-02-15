e3021d0def43cdcea8af151e7b84bc561b57e025
Fernando <mhfernando778@gmail.com>
2025-02-14 22:21:00 -0500

    Task 1 - Added Comments

--- cc_7.js
+++ cc_7.js
@@ -1,8 +1,9 @@
 // Task 1 - Customer Invoice Calculation //
- function calculateInvoice(subtotal, taxRate, discount) {
-    let total = (subtotal + (subtotal * taxRate)) - discount;
-    return total.toFixed(2);
+
+ function calculateInvoice(subtotal, taxRate, discount) { // starting a function to calculate invoice amount
+    let total = (subtotal + (subtotal * taxRate)) - discount; // the equation being used
+    return total.toFixed(2); // returning the calculation with 2 decimal places
  }
 
- console.log(`Final Invoice Amount: $${calculateInvoice(100, 0.08, 5)}`);
- console.log(`Final Invoice Amount: $${calculateInvoice(500, 0.1, 20)}`);
+ console.log(`Final Invoice Amount: $${calculateInvoice(100, 0.08, 5)}`); // logging the function with specified values
+ console.log(`Final Invoice Amount: $${calculateInvoice(500, 0.1, 20)}`); // logging the function with specified values
